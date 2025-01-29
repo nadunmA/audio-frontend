bdc63b331e97b77db369925feb4ee70b52978cd4
nadunmA <Naduncode404@outlook.com>
2025-01-29 20:37:16 +0530

    create login form👌🏻

--- kv-audio-fend/src/pages/login/login.jsx
+++ kv-audio-fend/src/pages/login/login.jsx
@@ -5,16 +5,36 @@ export default function LoginPage(){
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    function handleOnSubmit(e){
+        e.preventDefault()
+        console.log(email, password)
+    }
+
     return(
 
         <div className="bg-picture w-full h-screen flex justify-center items-center">
+           <form onSubmit={handleOnSubmit}>
 
-            <div className="w-[400px] h-[400px] backdrop-blur-xl rounded-l-2xl">
+              <div className="w-[400px] h-[400px] backdrop-blur-xl rounded-l-2xl flex justify-center items-center flex-col relative">
+              <img src="/kv.png" alt="logo" className="w-[80px] h-[80px] absolute top-4 object-cover"/>
+              <input type="email" placeholder="Email" className="w-[300px] h-[30px] mt-6 bg-transparent border-b-2 border-white text-white text-xl outline-none" 
+              value={email} onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              />
 
+              <input type="password" placeholder="Password" className="w-[300px] h-[30px] mt-6 bg-transparent border-b-2 border-white text-white text-xl outline-none" 
+              value={password} onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              />
 
+              <button className="my-8 w-[300px] h-[50px] bg-[#efac38] text-2xl text-white rounded-lg">Login</button>
 
-            </div>
+              </div>
 
+            </form>
         </div>
     )
 }
